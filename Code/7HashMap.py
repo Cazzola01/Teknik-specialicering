9838beca27d0304262b409e084315057e605e177
Marcus Cazzola <42636768+Cazzola01@users.noreply.github.com>
2020-10-20 15:06:05 +0200

    Fixed some errors. Now working
    Had to move the files to C: else debug did not work, when the file was located on ssis server

--- Code/7HashMap.py
+++ Code/7HashMap.py
@@ -1,4 +1,4 @@
-from LinkedList6 import LinkedList
+from Code.LinkedList6 import LinkedList
 
 class HashMap:
     def __init__(self, size):#Konstruktor
@@ -8,17 +8,16 @@ class HashMap:
     def AddLinkedLists(self):
         temp_list = []
         for i in range(self.size):
-            temp_list.append(LinkedList)
+            temp_list.append(LinkedList())
         return temp_list
 
-
-
-    def HashFunction(self, value): #Make value an int
+    def SetToInt(self, value): #Make value an int
         #if value.dtype() = int
         #if value.dtype() = float
         #if value.dtype() = string/char
 
-        key = value #Eftersom value är en int, så kan nykeln vara samma
+        #key = hash(value) #Eftersom value är en int, så kan nykeln vara samma
+        key = value
         return key
 
     def GetIndex(self, key):
@@ -26,11 +25,11 @@ class HashMap:
         return index
 
     def put(self, value):
-        key = self.HashFunction(self, value)
+        key = self.SetToInt(value)
         index = self.GetIndex(key)
         self.LinkedListArray[index].AddFirst(value)
 
-HashMap(size=6)
-HashMap.put(43)
+hashmap = HashMap(size=6)
+hashmap.put(43)
 
 
