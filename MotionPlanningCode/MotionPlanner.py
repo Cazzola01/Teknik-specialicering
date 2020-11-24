a185a4ca269690e6ffdb076d9dfd8ac5d19171b7
Marcus Cazzola <42636768+Cazzola01@users.noreply.github.com>
2020-11-24 14:58:59 +0100

    Draw fungerar!
    Den skriver ut både en cirkel och text

--- MotionPlanningCode/MotionPlanner.py
+++ MotionPlanningCode/MotionPlanner.py
@@ -1,4 +1,23 @@
 import json
-import MotionPlanningCode.Renderer as Renderer
+from MotionPlanningCode.Renderer import Renderer
+import pyglet
 
-json.load
+window = pyglet.window.Window(width=800, height=800)
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman', color=(0,255,255,255),
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+renderer = Renderer(window.width,window.height)
+renderer.add_render_object("Circle", [(200,600), 100], "circle", [0,0,0])
+#renderer.add_render_object("Triangle", "triangle2", [[100, 100], [300, 100], [700, 200]], color=(0,255,255,128))
+
+@window.event
+def on_draw():
+    window.clear()
+    renderer.draw()
+    label.draw()
+
+pyglet.app.run()
+
+
