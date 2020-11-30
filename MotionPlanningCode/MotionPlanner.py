bddd801bf3fab3bf99dcac487adb36013e0d253c
Marcus Cazzola <42636768+Cazzola01@users.noreply.github.com>
2020-11-30 10:12:13 +0100

    ploted points and loaded json file

--- MotionPlanningCode/MotionPlanner.py
+++ MotionPlanningCode/MotionPlanner.py
@@ -1,6 +1,12 @@
 import json
 from MotionPlanningCode.Renderer import Renderer
 import pyglet
+import random
+
+with open('obstaclesCircles.json') as json_file:
+    data = json.load(json_file)
+
+print(data)
 
 window = pyglet.window.Window(width=800, height=800)
 label = pyglet.text.Label('Hello, world',
@@ -8,9 +14,20 @@ label = pyglet.text.Label('Hello, world',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
-renderer = Renderer(window.width,window.height)
-renderer.add_render_object("Circle", [(200,600), 100], "circle", [0,0,0])
-#renderer.add_render_object("Triangle", "triangle2", [[100, 100], [300, 100], [700, 200]], color=(0,255,255,128))
+renderer = Renderer(window.width,window.height) #Lin window created
+
+def GenerateRandomTuplePoints(pointNum=100):
+    randomxlist = random.sample(range(0, 800), pointNum)
+    randomylist = random.sample(range(0, 800), pointNum)
+
+    pointList = []
+    for x in range(pointNum):
+        tuplePoint = (randomxlist[x], randomylist[x])
+        pointList.append(tuplePoint)
+    return pointList
+
+renderer.add_render_object("Circle", [(200,600), 100], "circle", [1,0,0])
+renderer.add_render_object("Point", GenerateRandomTuplePoints(pointNum=100), "points", [0,0,0])
 
 @window.event
 def on_draw():
